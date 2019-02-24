027d969b73faebc1f2c9a820a48c188b9b1170a7
Michael Phillips <michaeljoelphillips@gmail.com>
2019-02-24 01:19:20 -0600

    Add new TextDocuments on didOpen.

---
+++ src/LSP/Command/DidOpenTextDocument.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LanguageServer\LSP\Command;
+
+use LanguageServer\LSP\TextDocument;
+use LanguageServer\LSP\TextDocumentRegistry;
+use LanguageServer\RPC\Server;
+use React\Stream\WritableStreamInterface;
+
+/**
+ * @author Michael Phillips <michael.phillips@realpage.com>
+ */
+class DidOpenTextDocument
+{
+    private $registry;
+
+    public function __construct(Server $server, TextDocumentRegistry $registry)
+    {
+        $this->registry = $registry;
+
+        $server->on('textDocument/didOpen', [$this, 'handle']);
+    }
+
+    public function handle(object $request, WritableStreamInterface $output)
+    {
+        $textDocument = new TextDocument(
+            $request->params->textDocument->uri,
+            $request->params->textDocument->text,
+            $request->params->textDocument->version
+        );
+
+        $this->registry->add($textDocument);
+    }
+}
