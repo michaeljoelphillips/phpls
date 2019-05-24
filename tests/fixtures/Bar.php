1f0e9da764349f549f09b98d348c519bc98f25d7
Michael Phillips <michaeljoelphillips@gmail.com>
2019-05-23 22:37:56 -0500

    Add missing fixture file.

---
+++ tests/fixtures/Bar.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Fixtures;
+
+/**
+ * @author Michael Phillips <michael.phillips@realpage.com>
+ */
+class Bar
+{
+    public function __construct()
+    {
+    }
+}
