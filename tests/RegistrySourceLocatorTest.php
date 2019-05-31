f9b8933647a0132ca11f9ca083aa37a1d1a157f6
Michael Phillips <michaeljoelphillips@gmail.com>
2019-05-31 01:48:02 -0500

    Add a test for the RegistrySourceLocator.

---
+++ tests/RegistrySourceLocatorTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LanguageServer\Test;
+
+use LanguageServer\RegistrySourceLocator;
+use LanguageServer\TextDocumentRegistry;
+use PHPUnit\Framework\TestCase;
+use Roave\BetterReflection\Identifier\Identifier;
+use Roave\BetterReflection\Identifier\IdentifierType;
+use Roave\BetterReflection\Reflector\Reflector;
+use Roave\BetterReflection\SourceLocator\Ast\Locator;
+
+/**
+ * @author Michael Phillips <michael.phillips@realpage.com>
+ */
+class RegistrySourceLocatorTest extends TestCase
+{
+    private $subject;
+    private $registry;
+
+    public function setUp(): void
+    {
+        $astLocator = $this->createMock(Locator::class);
+        $this->registry = $this->createMock(TextDocumentRegistry::class);
+
+        $this->subject = new RegistrySourceLocator($astLocator, $this->registry);
+    }
+
+    public function testLocateIdentifier()
+    {
+        $this
+            ->registry
+            ->expects($this->once())
+            ->method('getAll')
+            ->willReturn([]);
+
+        $reflector = $this->createMock(Reflector::class);
+
+        $this->subject->locateIdentifier($reflector, new Identifier('Foo', new IdentifierType()));
+    }
+
+    public function testLocateIdentifiersByType()
+    {
+        $this
+            ->registry
+            ->expects($this->once())
+            ->method('getAll')
+            ->willReturn([]);
+
+        $reflector = $this->createMock(Reflector::class);
+
+        $this->subject->locateIdentifiersByType($reflector, new IdentifierType());
+    }
+}
