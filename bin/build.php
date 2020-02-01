a7974bd58da8efe26718f10eac6fd53f617ca3f2
Michael Phillips <michaeljoelphillips@gmail.com>
2020-01-31 23:40:59 -0600

    Prepare build scripts for release

---
+++ bin/build.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php
+
+define('SRC_PATH', __DIR__.'/../src');
+define('PROJECT_ROOT', __DIR__.'/..');
+define('VENDOR_PATH', __DIR__.'/../vendor');
+define('BUILD_PATH', __DIR__.'/../build/phpls.phar');
+
+if (file_exists(BUILD_PATH)) {
+    unlink(BUILD_PATH);
+}
+
+if (file_exists(BUILD_PATH.'.gz')) {
+    unlink(BUILD_PATH.'.gz');
+}
+
+$builder = new Phar(BUILD_PATH);
+
+$builder->buildFromIterator(new RecursiveIteratorIterator(new RecursiveDirectoryIterator(SRC_PATH)), PROJECT_ROOT);
+$builder->buildFromIterator(new RecursiveIteratorIterator(new RecursiveDirectoryIterator(VENDOR_PATH)), PROJECT_ROOT);
+
+$builder->addFile('bin/language-server.php');
+$builder->setStub($builder->createDefaultStub('bin/language-server.php'));
