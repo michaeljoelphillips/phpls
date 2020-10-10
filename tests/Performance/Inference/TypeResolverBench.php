239544542ae98f9f44d1a54c7d248944634bac01
Michael Phillips <michaeljoelphillips@gmail.com>
2020-10-09 23:21:32 -0500

    Add first performance test

---
+++ tests/Performance/Inference/TypeResolverBench.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LanguageServer\Test\Performance\Inference;
+
+use DI\ContainerBuilder;
+use LanguageServer\Inference\TypeResolver;
+use LanguageServer\ParsedDocument;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
+use PhpParser\Parser;
+use function file_get_contents;
+
+class TypeResolverBench
+{
+    private TypeResolver $subject;
+    private Parser $parser;
+
+    public function setUp() : void
+    {
+        $container = (new ContainerBuilder())
+            ->addDefinitions(__DIR__ . '/../../../src/services.php')
+            ->build();
+
+        $container->set('project_root', '/home/nomad/Code/hermes');
+
+        $this->parser  = $container->get(Parser::class);
+        $this->subject = $container->get(TypeResolver::class);
+    }
+
+    /**
+     * @BeforeMethods({"setUp"})
+     */
+    public function benchGetTypeOnPHPUnitMockObject() : void
+    {
+        $source   = file_get_contents('/home/nomad/Code/hermes/tests/Unit/Twitch/SerializedTokenStorageTest.php');
+        $nodes    = $this->parser->parse($source);
+        $document = new ParsedDocument('file:///tmp/file.php', $source, $nodes);
+
+        $node = new MethodCall(
+            new MethodCall(
+                new Variable('this'),
+                new Identifier('createMock')
+            ),
+            new Identifier('expects')
+        );
+
+        $this->subject->getType($document, $node);
+    }
+}
