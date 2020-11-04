b7d1951e90010970317b1205fd80e26a89dbbc2d
Michael Phillips <michaeljoelphillips@gmail.com>
2020-11-03 20:08:02 -0600

    Fix broken tests for Did* methods

--- tests/Unit/MessageHandler/TextDocument/DidChangeTest.php
+++ tests/Unit/MessageHandler/TextDocument/DidChangeTest.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace LanguageServer\Test\Unit\MessageHandler\TextDocument;
 
 use LanguageServer\MessageHandler\TextDocument\DidChange;
+use LanguageServer\ParsedDocument;
+use LanguageServer\Parser\DocumentParser;
 use LanguageServer\Server\Protocol\NotificationMessage;
 use LanguageServer\TextDocumentRegistry;
-use PhpParser\Parser;
 use PHPUnit\Framework\TestCase;
 
 class DidChangeTest extends TestCase
@@ -15,12 +16,15 @@ class DidChangeTest extends TestCase
     public function testDidChange(): void
     {
         $registry = $this->createMock(TextDocumentRegistry::class);
-        $parser   = $this->createMock(Parser::class);
+        $parser   = $this->createMock(DocumentParser::class);
+        $subject  = new DidChange($registry, $parser);
+        $document = new ParsedDocument('file:///tmp/foo.php', '<?php echo "Hi";?>', []);
 
         $parser
             ->expects($this->once())
             ->method('parse')
-            ->willReturn([]);
+            ->with('file:///tmp/foo.php', '<?php echo "Hi";?>')
+            ->willReturn($document);
 
         $registry
             ->expects($this->once())
@@ -36,12 +40,8 @@ class DidChangeTest extends TestCase
             ],
         ]);
 
-        $next = function (): void {
+        $subject->__invoke($request, function (): void {
             $this->fail('The next method should never be called');
-        };
-
-        $subject = new DidChange($registry, $parser);
-
-        $subject->__invoke($request, $next);
+        });
     }
 }
--- tests/Unit/MessageHandler/TextDocument/DidSaveTest.php
+++ tests/Unit/MessageHandler/TextDocument/DidSaveTest.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace LanguageServer\Test\Unit\MessageHandler\TextDocument;
 
 use LanguageServer\MessageHandler\TextDocument\DidSave;
+use LanguageServer\ParsedDocument;
+use LanguageServer\Parser\DocumentParser;
 use LanguageServer\Server\Protocol\NotificationMessage;
 use LanguageServer\TextDocumentRegistry;
-use PhpParser\Parser;
 use PHPUnit\Framework\TestCase;
 
 class DidSaveTest extends TestCase
@@ -15,27 +16,25 @@ class DidSaveTest extends TestCase
     public function testDidSave(): void
     {
         $registry = $this->createMock(TextDocumentRegistry::class);
-        $parser   = $this->createMock(Parser::class);
+        $parser   = $this->createMock(DocumentParser::class);
         $subject  = new DidSave($registry, $parser);
+        $document = new ParsedDocument('file:///tmp/foo.php', '<?php echo "Hi";?>', []);
+
+        $parser
+            ->method('parseFromFile')
+            ->willReturn($document);
 
         $registry
             ->expects($this->once())
-            ->method('add');
-
-        $parser
-            ->method('parse')
-            ->willReturn([]);
+            ->method('add')
+            ->with($document);
 
         $request = new NotificationMessage('textDocument/didSave', [
-            'textDocument' => [
-                'uri' => __DIR__ . '/../../../fixtures/ParsedDocumentFixture.php',
-            ],
+            'textDocument' => ['uri' => 'file:///tmp/foo.php'],
         ]);
 
-        $next = function (): void {
+        $subject->__invoke($request, function (): void {
             $this->fail('The next method should never be called');
-        };
-
-        $subject->__invoke($request, $next);
+        });
     }
 }
